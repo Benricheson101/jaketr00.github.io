f203a6922f0137da2a1acac22378e1b3c781fa7b
jaketr00 <jaketr00@gmail.com>
2016-08-29 12:07:57 -0400

    Biblegateway.com mod hosting

---
+++ website_mods/biblegateway.com.js
@@ -0,0 +1,140 @@
+// ==UserScript==
+// @name         Auto Capitalize Text/More
+// @version      1.0
+// @description  Auto capitalize/More text in search field
+// @author       Jake Rosch
+// @match        *://www.biblegateway.com/*
+// @grant        none
+// @downloadURL  http://jaketr00.github.io/website_mods/biblegateway.com.js
+// @updateURL    http://jaketr00.github.io/website_mods/biblegateway.com.js
+// ==/UserScript==
+
+
+(function() {
+    var shortcut = {
+        //_pr: 'Proverbs'
+    };
+    addEventListener('load', function() {
+
+        function doGetCaretPosition(oField) {
+
+            // Initialize
+            var iCaretPos = 0;
+
+            // IE Support
+            if (document.selection) {
+
+                // Set focus on the element
+                oField.focus();
+
+                // To get cursor position, get empty selection range
+                var oSel = document.selection.createRange();
+
+                // Move selection start to 0 position
+                oSel.moveStart('character', -oField.value.length);
+
+                // The caret position is selection length
+                iCaretPos = oSel.text.length;
+            }
+
+            // Firefox support
+            else if (oField.selectionStart || oField.selectionStart == '0')
+                iCaretPos = oField.selectionStart;
+
+            // Return results
+            return iCaretPos;
+        }
+
+        function setCaretPosition(elem, caretPos) {
+
+            if (elem !== null) {
+                if(elem.createTextRange) {
+                    var range = elem.createTextRange();
+                    range.move('character', caretPos);
+                    range.select();
+                }
+                else {
+                    if(elem.selectionStart) {
+                        elem.focus();
+                        elem.setSelectionRange(caretPos, caretPos);
+                    }
+                    else
+                        elem.focus();
+                }
+            }
+        }
+
+        function setVal(e) {
+            setTimeout(function() {
+
+                var pos = doGetCaretPosition(document.querySelector('#quicksearch-field'));
+
+                var value = document.querySelector('#quicksearch-field').value.replace(/[\s ]\w|^\w/g, function(char) {
+                    return char.toUpperCase();
+                });
+
+                if (value.match(/\d+([: ]\d+)([\- ]\d+){0,1}/)) {
+
+                    var match = value.match(/\d+([: ]\d+)([\- ]\d+){0,1}/g),
+                        allReplaces = [];
+
+                    for (var a = 0; match.length > a; a++) {
+
+                        var numbers = match[a].replace(/:{2,}/g, ':').replace(/-{2,}/, '-').replace(/ {2,}/, ' ').split(/ |-|:|, |,/),
+                        numbersJoined = '';
+
+                        for (var i = 0; numbers.length > i; i++) {
+                            if (i === 1)
+                                numbersJoined+= ':';
+                            else if (i === 2)
+                                numbersJoined+= '-';
+                            numbersJoined+= numbers[i];
+                        }
+
+                        allReplaces.push(numbersJoined);
+
+                    }
+
+                    var num = 0;
+                    value = value.replace(/\d+([: ]\d+)([\- ]\d+){0,1}/g, function() {
+
+                        num++;
+
+                        return allReplaces[num-1];
+
+                    });
+
+                }
+
+                value = value.replace(/\d [\d\w]/g, function(c) {
+
+                    pos++;
+                    return c.replace(' ', ', ');
+
+                });
+
+                if (e && (e === 'alreadyVals' || e.type === 'paste' || e.keyCode === 32))
+                    value = value.replace(/\w+/g, function(chars) {
+                        var charsLow = chars.toLowerCase();
+                        if (shortcut['_'+charsLow]) {
+                            pos+= shortcut['_'+charsLow].length - charsLow.length;
+                            return shortcut['_'+charsLow];
+                        }
+                        return chars;
+                    });
+
+                document.querySelector('#quicksearch-field').value = value;
+
+                setCaretPosition(document.querySelector('#quicksearch-field'), pos);
+
+            }, 1);
+        }
+
+        document.querySelector('#quicksearch-field').addEventListener('keypress', setVal);
+        document.querySelector('#quicksearch-field').addEventListener('paste', setVal);
+
+        if (document.querySelector('#quicksearch-field').value)
+            setVal('alreadyVals');
+
+    });
+})();
